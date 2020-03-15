11038eb885c5a9ca52c82c1f5193e37f1ac19d05
Goran Devic <gdevic@yahoo.com>
2020-03-15 12:55:00 -0500

    Save and restore window position and size

--- src/main.cpp
+++ src/main.cpp
@@ -25,11 +25,23 @@ int main(int argc, char *argv[])
         // Main initialization outside the constructor
         while (mainWindow->init())
         {
+            QSettings settings;
+
+            // Load and set main window location and size
+            // Include also all docking windows location, size and docking status
+            mainWindow->restoreGeometry(settings.value("MainWindow/Geometry").toByteArray());
+            mainWindow->restoreState(settings.value("MainWindow/State").toByteArray());
+
             // Show the main window
             mainWindow->show();
 
             // Run the application main code loop
             retCode = a.exec();
+
+            // Save window configuration after the main application finished executing
+            // Include also all docking windows location, size and docking status
+            settings.setValue("MainWindow/State", mainWindow->saveState());
+            settings.setValue("MainWindow/Geometry", mainWindow->saveGeometry());
 
             break;
         }
