c94e412954581cc8050a53dda5d06604a86b28cb
Goran Devic <gdevic@yahoo.com>
2020-04-13 22:26:39 -0500

    Adding a pretty nice starting config

---
+++ resource/netnames.js
@@ -0,0 +1,10 @@
+// This file contains custom net names, overrides of the names defined in nodenames.js
+// and definitions of buses (collections of nets). Modify by hand only when the app is not running.
+var nodenames_override = {
+// Buses:
+AB: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+DB: [32,33,34,35,36,37,38,39],
+INSTR: [1348,1359,1365,1379,1387,1394,1369,1374],
+M: [155,173,163,159,209,210],
+T: [115,137,144,166,134,168],
+}
