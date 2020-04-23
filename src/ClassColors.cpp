34f606979e921871a2669aed19cf4e8a5b8a6d15
Goran Devic <gdevic@yahoo.com>
2020-04-23 17:12:19 -0500

    Minor

--- src/ClassColors.cpp
+++ src/ClassColors.cpp
@@ -4,6 +4,6 @@ ClassColors::ClassColors(QObject *parent) : QObject(parent)
 {
     // Populate some known colors
     m_colors[1] = QColor(  0, 127,   0); // vss dark green
-    m_colors[2] = QColor(255,   0,   0); // vcc red
+    m_colors[2] = QColor(192,   0,   0); // vcc red
     m_colors[3] = QColor(200, 200, 200); // clk white-ish
 }
