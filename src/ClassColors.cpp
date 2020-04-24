3dc222608522395cfd784347281f88224f10ace3
Goran Devic <gdevic@yahoo.com>
2020-04-24 11:30:14 -0500

    Clks is subdued white

--- src/ClassColors.cpp
+++ src/ClassColors.cpp
@@ -6,5 +6,5 @@ ClassColors::ClassColors(QObject *parent) : QObject(parent)
     m_colors[0] = QColor(Qt::black);        // Also used as an invalid color
     m_colors[1] = QColor(  0, 127,   0);    // vss medium green
     m_colors[2] = QColor(192,   0,   0);    // vcc quite red
-    m_colors[3] = QColor(Qt::white);        // clk
+    m_colors[3] = QColor(192,192,192);      // clk subdued white
 }
