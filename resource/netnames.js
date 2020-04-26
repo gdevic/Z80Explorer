350ed997e5f2294cf471f0bb31aee10424baeb84
Goran Devic <gdevic@yahoo.com>
2020-04-26 00:29:49 -0500

    Adding abus to the net names

--- resource/netnames.js
+++ resource/netnames.js
@@ -1,6 +1,22 @@
 // This file contains custom net names, overrides of the names defined in nodenames.js
 // and definitions of buses (collections of nets). Modify by hand only when the app is not running.
 var nodenames_override = {
+abus0: 3370,
+abus1: 3381,
+abus2: 1968,
+abus3: 1970,
+abus4: 792,
+abus5: 797,
+abus6: 2160,
+abus7: 2163,
+abus8: 2279,
+abus9: 2280,
+abus10: 2396,
+abus11: 921,
+abus12: 954,
+abus13: 957,
+abus14: 1003,
+abus15: 1004,
 // Buses:
 AB: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
 DB: [32,33,34,35,36,37,38,39],
