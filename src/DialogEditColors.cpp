7e8f66a941bcf3a76bc6375d4c1192d0b71768ed
Goran Devic <gdevic@yahoo.com>
2020-05-13 18:14:39 -0500

    Edit colors to save its position and size

--- src/DialogEditColors.cpp
+++ src/DialogEditColors.cpp
@@ -1,14 +1,21 @@
 #include "DialogEditColors.h"
 #include "ui_DialogEditColors.h"
+#include <QSettings>
 
 DialogEditColors::DialogEditColors(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogEditColors)
 {
     ui->setupUi(this);
+    setWindowFlag(Qt::WindowContextHelpButtonHint, false);
+    QSettings settings;
+    restoreGeometry(settings.value("editColorsGeometry").toByteArray());
 }
 
 DialogEditColors::~DialogEditColors()
 {
+    QSettings settings;
+    settings.setValue("editColorsGeometry", saveGeometry());
+
     delete ui;
 }
