20edaf4159c840319b4bc8583d6e0c41e3c58979
Goran Devic <gdevic@yahoo.com>
2020-05-25 21:20:10 -0500

    Updating release script

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 set RELEASEDIR=Z80Explorer
 set VCINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
 
-set path=C:\Qt\5.14.1\msvc2015_64\bin;%VCINSTALLDIR%;%path%
+set path=C:\Qt\5.14.2\msvc2015_64\bin;%VCINSTALLDIR%;%path%
 
 call vcvarsall.bat
 
@@ -13,19 +13,22 @@ mkdir %RELEASEDIR%\platforms
 mkdir %RELEASEDIR%\styles
 mkdir %RELEASEDIR%\resource
 
-copy "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\vccorlib140.dll  %RELEASEDIR%
-copy "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\vcruntime140.dll %RELEASEDIR%
-copy "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\msvcp140.dll     %RELEASEDIR%
+xcopy /Y "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\vccorlib140.dll  %RELEASEDIR%
+xcopy /Y "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\vcruntime140.dll %RELEASEDIR%
+xcopy /Y "%VCINSTALLDIR%"\redist\x64\Microsoft.VC140.CRT\msvcp140.dll     %RELEASEDIR%
 rem copy vcredist_x64.exe %RELEASEDIR%
 
-copy Z80Explorer.exe  %RELEASEDIR%
-copy highDPI.bat      %RELEASEDIR%
-copy Qt5Core.dll      %RELEASEDIR%
-copy Qt5Gui.dll       %RELEASEDIR%
-copy Qt5Script.dll    %RELEASEDIR%
-copy Qt5Widgets.dll   %RELEASEDIR%
-copy platforms\qwindows.dll %RELEASEDIR%\platforms
-copy styles\qwindowsvistastyle.dll %RELEASEDIR%\styles
-copy resource\*       %RELEASEDIR%\resource
+xcopy /Y Z80Explorer.exe  %RELEASEDIR%
+xcopy /Y highDPI.bat      %RELEASEDIR%
+xcopy /Y cleanup.reg      %RELEASEDIR%
+xcopy /Y Qt5Core.dll      %RELEASEDIR%
+xcopy /Y Qt5Gui.dll       %RELEASEDIR%
+xcopy /Y Qt5Script.dll    %RELEASEDIR%
+xcopy /Y Qt5Widgets.dll   %RELEASEDIR%
+xcopy /Y platforms\qwindows.dll %RELEASEDIR%\platforms
+xcopy /Y styles\qwindowsvistastyle.dll %RELEASEDIR%\styles
+
+rem Assumes Z80Explorer_Z80 resource files are at the same level as this project
+xcopy /Y /E ..\Z80Explorer_Z80\*  %RELEASEDIR%\resource
 
 :end
