d71dc1feab93324ac6d0eb8e0e42e8d2256287a5
Goran Devic <gdevic@yahoo.com>
2020-05-27 00:16:16 -0500

    Adding the startup script init.js

---
+++ resource/init.js
@@ -0,0 +1,6 @@
+// Z80Explorer loads this init script on a startup
+
+// Load "Hello, World" program into the simulated memory
+
+script.response("Loading example Z80 program");
+monitor.loadHex("hello_world.hex");
