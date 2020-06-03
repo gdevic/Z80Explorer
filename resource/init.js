6d096d43a03d1a5592d7302784794a7d8d70fe0b
Goran Devic <gdevic@yahoo.com>
2020-06-03 15:53:35 -0500

    Minor

--- resource/init.js
+++ resource/init.js
@@ -2,5 +2,5 @@
 
 // Load "Hello, World" program into the simulated memory
 
-script.response("Loading example Z80 program\n");
+script.response("Loading 'Hello, world' Z80 program\n");
 monitor.loadHex("hello_world.hex");
