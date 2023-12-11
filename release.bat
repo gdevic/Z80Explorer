acc917bfd1d859ec62a384b55090e76308902768
Goran Devic <gdevic@yahoo.com>
2023-12-10 20:49:37 -0600

    Simplify the release script

--- release.bat
+++ release.bat
@@ -1,49 +1,37 @@
 @REM Relase script specific to Visual Studio 2019 and Qt 5.15.2
-set RELEASEDIR=Z80Explorer
+@REM Z80Explorer.exe must be precompiled and stored in the root project folder
 if not exist "Z80Explorer.exe" Goto end
-mkdir release
-xcopy /Y Z80Explorer.exe release
-cd release
 
 @REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
 set path=C:\Qt\5.15.2\msvc2019_64\bin;%VCINSTALLDIR%;%path%
 
+mkdir release
+cd release
+xcopy /Y ..\Z80Explorer.exe .
+xcopy /Y ..\highDPI.bat .
+xcopy /Y ..\cleanup.reg .
+xcopy /Y ..\Z80Explorer.pdf .
+
 windeployqt.exe --compiler-runtime Z80Explorer.exe
 if errorlevel 1 goto end
 
-mkdir %RELEASEDIR%
-mkdir %RELEASEDIR%\platforms
-mkdir %RELEASEDIR%\styles
-
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vccorlib140.dll    %RELEASEDIR%
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vcruntime140.dll   %RELEASEDIR%
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vcruntime140_1.dll %RELEASEDIR%
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140.dll       %RELEASEDIR%
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140_1.dll     %RELEASEDIR%
-xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140_2.dll     %RELEASEDIR%
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vccorlib140.dll    .
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vcruntime140.dll   .
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\vcruntime140_1.dll .
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140.dll       .
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140_1.dll     .
+xcopy /Y "%VCToolsRedistDir%"\x64\Microsoft.VC142.CRT\msvcp140_2.dll     .
 REM xcopy /Y "%VCToolsRedistDir%"\vcredist_x64.exe %RELEASEDIR%
 
-xcopy /Y Z80Explorer.exe  %RELEASEDIR%
-xcopy /Y highDPI.bat      %RELEASEDIR%
-xcopy /Y cleanup.reg      %RELEASEDIR%
-xcopy /Y Qt5Core.dll      %RELEASEDIR%
-xcopy /Y Qt5Gui.dll       %RELEASEDIR%
-xcopy /Y Qt5Script.dll    %RELEASEDIR%
-xcopy /Y Qt5Network.dll   %RELEASEDIR%
-xcopy /Y Qt5Widgets.dll   %RELEASEDIR%
-xcopy /Y platforms\qwindows.dll %RELEASEDIR%\platforms
-xcopy /Y styles\qwindowsvistastyle.dll %RELEASEDIR%\styles
+mkdir resource
+xcopy /Y /S ..\resource resource
 
-@echo Download Z80 resources from a public git repo...
-cd %RELEASEDIR%
-git clone --depth=1 --branch=master https://github.com/gdevic/Z80Explorer_Z80.git resource
-rmdir /S /Q "resource/".git"
+rmdir /S /Q bearer iconengines imageformats translations
+
 cd ..
-del *.dll Z*.exe
-rmdir /S /Q bearer iconengines imageformats platforms styles translations
 
 @echo.
-@echo Release files are in Z80Explorer folder
+@echo DONE
 :end
