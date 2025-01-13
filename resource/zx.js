ab6d47d09a9fb2934e157051ddcb5e620ca15da1
Goran Devic <gdevic@yahoo.com>
2025-01-12 21:47:55 -0600

    Improved zx.js file to exit if it can't find the rom file

--- resource/zx.js
+++ resource/zx.js
@@ -3,11 +3,24 @@
 // wait patiently - it takes approximately 20 min to get to that point when the
 // simulation is running at around 4kHz.
 //
-mon.loadBin("48.rom", 0);
-mon.rom = 16384;
-mon.enabled = 0;
-mon.zx();
-reset();
-run(0);
-print("\nFor a faster simulation, remove all nets from Watchlist (Items to be tracked).");
-print("It takes about 20 minutes to get to the initial screen when running at around 4kHz.");
+function ZXSpectrum()
+{
+   if (mon.loadBin("48.rom", 0))
+   {
+      mon.rom = 16384;
+      mon.enabled = 0;
+      mon.zx();
+      reset();
+      run(0);
+      print("\nZX Spectrum simulation is running.")
+      print("For a faster simulation, remove all nets from Watchlist and close Waveform views.");
+      print("It takes about 20 minutes to get to the initial screen when running at around 4kHz.");
+   }
+   else
+   {
+      print("ERROR: Unable to load 48.rom\n");
+      return(0);
+   }
+}
+
+ZXSpectrum();
